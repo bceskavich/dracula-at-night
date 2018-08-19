62785dfe4c6f3a33bb725bd95c87faead0410641
Billy Ceskavich <billy.ceskavich@voxmedia.com>
2018-08-19 17:52:10 -0400

    Bugfix: string punctuation

--- src/settings/tokens/general/strings.ts
+++ src/settings/tokens/general/strings.ts
@@ -4,7 +4,17 @@ import TokenSettings from '../TokenSettings';
 export default ({ base }: Colors): TokenSettings[] => [
   {
     name: 'Strings',
-    scope: ['string'],
+    scope: [
+      'string',
+
+      // For some reason, quotation marks surrounding strings will not be styled
+      // yellow when the string is used as a function parameter or object property
+      // in some languages. So, we force the styling explicitly!
+      'punctuation.definition.string.begin',
+      'punctuation.definition.string.end',
+      'punctuation.support.type.property-name.begin',
+      'punctuation.support.type.property-name.end'
+    ],
     settings: {
       foreground: base.yellow
     }
