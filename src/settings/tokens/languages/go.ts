a733b9967667c21065185da0e30160aad94269f1
Billy Ceskavich <bceskavich@gmail.com>
2019-08-04 16:52:06 -0400

    Add some more syntax coloring definitions for Go

--- src/settings/tokens/languages/go.ts
+++ src/settings/tokens/languages/go.ts
@@ -1,12 +1,48 @@
 import { Colors } from '../../Theme';
 import TokenSettings from '../TokenSettings';
 
-export default ({ base }: Colors): TokenSettings[] => [
-  {
-    name: 'Go Types',
-    scope: ['source.go storage.type'],
-    settings: {
-      foreground: base.cyan
-    }
-  }
+export default (colors: Colors): TokenSettings[] => [
+  punctuation(colors),
+  ...typings(colors),
+  ...functions(colors)
 ];
+
+function typings({ base }: Colors): TokenSettings[] {
+  return [
+    {
+      name: 'Go Types',
+      scope: ['source.go storage.type'],
+      settings: {
+        foreground: base.cyan
+      }
+    },
+    {
+      name: 'Go Types',
+      scope: ['entity.name.type.go'],
+      settings: { foreground: base.purple }
+    }
+  ];
+}
+
+function functions({ base }: Colors): TokenSettings[] {
+  return [
+    {
+      name: 'Go function definitions',
+      scope: ['entity.name.function.go'],
+      settings: { foreground: base.green }
+    },
+    {
+      name: 'Go function invocation',
+      scope: ['support.function.go'],
+      settings: { foreground: base.cyan }
+    }
+  ];
+}
+
+function punctuation({ base }: Colors): TokenSettings {
+  return {
+    name: 'Go separators',
+    scope: ['punctuation.other.period.go', 'punctuation.other.colon.go'],
+    settings: { foreground: base.pink }
+  };
+}
